6f326335cb566ac0864c405b030f3cb0dbc53a4d
Roni Sarkar <ronichandrasarkar@gmail.com>
2025-01-11 11:33:25 +0530

    changes deviation success message

--- src/app/modules/cryptocurrency/cryptocurrency.controller.ts
+++ src/app/modules/cryptocurrency/cryptocurrency.controller.ts
@@ -21,7 +21,7 @@ const getDeviation = async (req: Request, res: Response) => {
   try {
     const result = await StatsService.getDeviation(req.body);
     res.status(200).json({
-      message: "Coin get successfully!",
+      message: "Deviation get successfully!",
       success: true,
       data: result,
     });
